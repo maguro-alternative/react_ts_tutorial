716f769fe33a8e55e703b6f91c975c6d13ed9e21
ygo <ka.ra.si.naporitan@gmail.com>
2022-04-12 10:13:09 +0900

    master

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 //import './App.css'
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 function Square(props:any) {
   return (
@@ -22,8 +22,11 @@ class Board extends React.Component <{}, { squares:any , xIsNext:boolean}>{
     };
   }
 
+  //マスをクリックした際のアクション
   handleClick(i:number) {
+    // squaresの中身をすべて取得
     const squares = this.state.squares.slice();
+    // 勝者が確定、すでにOXがある場合
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -34,6 +37,7 @@ class Board extends React.Component <{}, { squares:any , xIsNext:boolean}>{
     });
   }
 
+  //1マスの情報を示すクラス
   renderSquare(i:number) {
     return (
       <Square
@@ -41,6 +45,17 @@ class Board extends React.Component <{}, { squares:any , xIsNext:boolean}>{
         onClick={() => this.handleClick(i)}
       />
     );
+  }
+
+  // マス目の情報をリセット
+  resetClick(){
+      for(let i=0; i<10; i++){
+        this.state.squares[i]=null
+        this.handleClick(i)
+      }
+      this.setState({
+        xIsNext: false
+      });
   }
 
   render() {
@@ -70,6 +85,7 @@ class Board extends React.Component <{}, { squares:any , xIsNext:boolean}>{
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <button onClick={() => this.resetClick()}>{"reset"}</button>
       </div>
     );
   }
@@ -80,6 +96,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
+          {/*boardクラスへ*/}
           <Board />
         </div>
         <div className="game-info">
@@ -93,7 +110,10 @@ class Game extends React.Component {
 
 // ========================================
 
+
+// reactのMain関数
 ReactDOM.render(
+  // Gameクラスヘ
   <Game />,
   document.getElementById('root')
 );
@@ -128,10 +148,154 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+/*function Square(props:any) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component <{}, { squares:[] , xIsNext:boolean }>{
+  renderSquare(i:number) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component <{}, { squares:[] , history:any , xIsNext:boolean , stepNumber:number }>{
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i:number) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step:any) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step:any, move:any) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares:any) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 */
